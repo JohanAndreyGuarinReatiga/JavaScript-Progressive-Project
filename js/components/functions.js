81b853e418345db1c161502b35e19f15750dc713
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-04-08 21:40:00 -0500

    feat: :sparkles: data variablesd and api configuartion

--- js/components/functions.js
+++ js/components/functions.js
@@ -6,8 +6,18 @@ const ipDisplay = document.getElementById("ip-display")
 const historyList = document.getElementById("history-list")
 const clearHistoryBtn = document.getElementById("clear-history-btn")
 
-// const KEY = "at_JJ1eX1RGQCZ6t8hElZf6EB5iPrI3x";
-// const url = new URL("https://geo.ipify.org/api/v2/country?apiKey=")
+// data vars
+const countryElement = document.getElementById("country")
+const regionElement = document.getElementById("region")
+const stateElement = document.getElementById("state")
+const cityElement = document.getElementById("city")
+const postalElement = document.getElementById("postal")
+const latitudeElement = document.getElementById("latitude")
+const longitudeElement = document.getElementById("longitude")
+const timezoneElement = document.getElementById("timezone")
+
+const KEY = "at_JJ1eX1RGQCZ6t8hElZf6EB5iPrI3x";
+const URL = "https://geo.ipify.org/api/v2/country"
 
 
 
