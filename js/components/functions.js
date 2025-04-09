26f9c4d2ec9fec16dc36d7cfefb33d0bd13e4c1f
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-04-08 21:34:52 -0500

    feat: :construction: dom variables added

---
+++ js/components/functions.js
@@ -0,0 +1,14 @@
+const ipInput = document.getElementById("ip-input")
+const searchBtn = document.getElementById("search-btn")
+const currentLocationBtn = document.getElementById("current-location-btn")
+const resultCard = document.getElementById("result-card")
+const ipDisplay = document.getElementById("ip-display")
+const historyList = document.getElementById("history-list")
+const clearHistoryBtn = document.getElementById("clear-history-btn")
+
+// const KEY = "at_JJ1eX1RGQCZ6t8hElZf6EB5iPrI3x";
+// const url = new URL("https://geo.ipify.org/api/v2/country?apiKey=")
+
+
+
+
